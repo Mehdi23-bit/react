67f57002792cfcae3a03dc53c695d103e2125377
mehdi <elmehdiiskandar3@gmail.com>
2025-02-01 00:31:01 +0100

    initial commit

--- src/Test.js
+++ src/Test.js
@@ -1,42 +1,70 @@
 import './Test.css';
 function Test(props){
     console.log(props.container,props.cell)
-    
-//     let cells=document.getElementsByClassName('cell')
-//     cells.array.forEach(cell => {
-//        cell.addEventListener('click',(e)=>{
-//  console.log('you can do it man')
-//        }) 
-//     });
-let turns=['X','O']
-let turn;
- window.addEventListener('DOMContentLoaded',()=>{
-let turn=turns[Math.floor(Math.random()*10)%2]
- })
-
+let turn='X';
+let game=[['#','#','#'],
+          ['#','#','#'],
+          ['#','#','#']]
 const handleClick = (e) => {
-    // A ra
+    
     if(e.target.textContent===''){
         e.target.textContent=turn
  turn=(turn==='X')?'O':'X';
-    }
-   
-};
-
-    return(<div className={props.container}><div className={props.cell} onClick={handleClick}></div>
-                <div className={props.cell} onClick={handleClick}></div>
-                <div className={props.cell} onClick={handleClick}></div>
-                <div className={props.cell} onClick={handleClick}></div>
-                <div className={props.cell} onClick={handleClick}></div>
-                <div className={props.cell} onClick={handleClick}></div>
-                <div className={props.cell} onClick={handleClick}></div>
-                <div className={props.cell} onClick={handleClick}></div>
-                <div className={props.cell} onClick={handleClick}></div>
-                
-    </div>);
-   } 
- function Button(){
-    return(<button>btn</button>)
+ document.getElementsByClassName('turn')[0].textContent=`Player ${turn}'s turn`
+ let index=indexOf(document.getElementsByClassName('cell'),e.target)
+ let x=Math.floor(index/3)
+ let y=index%3
+ console.log(x,y)
+game[x][y]=e.target.textContent
+if(isWon(x,y)){
+    console.log("win")
 }
+console.log(game)
+       }
+   };
+   function indexOf(array,object){
+    for(let i=0;i<array.length;i++){
+        if(array[i]==object)
+            return i;
+    }
+   }
+   function isWon(x,y){
+return check(game[x] || check(extract(y,game)))
+    
+}
+  
+   function check(array){
+    for(let i=0;i<array.length-1;i++){
+        if(array[i]!=array[i+1]){
+            return false;
+        }
+         }
+         return true
+        }
+   function extract(y,array){
+    let newarry;
+    for(let i=0;i<array.length;i++){
+         newarry.push(array[i][y]) 
+           }
+         return newarry;
+    }
+
+         return(<div className='tic_tac'>
+            <h1>Tic Tac Toe</h1>
+            <p className='turn'>Player {turn}'s turn</p>
+                <div className={props.container}>
+                <div className={props.cell} onClick={handleClick}></div>
+                <div className={props.cell} onClick={handleClick}></div>
+                <div className={props.cell} onClick={handleClick}></div>
+                <div className={props.cell} onClick={handleClick}></div>
+                <div className={props.cell} onClick={handleClick}></div>
+                <div className={props.cell} onClick={handleClick}></div>
+                <div className={props.cell} onClick={handleClick}></div>
+                <div className={props.cell} onClick={handleClick}></div>
+                <div className={props.cell} onClick={handleClick}></div>
+                </div>
+                </div>);
+                }
+    
 export default Test;
 
