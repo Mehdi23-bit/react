e4b5fe874e0686abd3fad212f45b0ff3d0b19131
mehdi <elmehdiiskandar3@gmail.com>
2025-01-31 19:51:19 +0100

    commit

---
+++ src/Test.js
@@ -0,0 +1,17 @@
+function Test(){
+    return(<div><div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>v
+    </div>);
+} 
+ function Button(){
+    return(<button>btn</button>)
+}
+export default Test;
+
