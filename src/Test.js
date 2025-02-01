29aa0b13e251e698acc50c2e4eda306194c5249e
mehdi <elmehdiiskandar3@gmail.com>
2025-02-01 11:05:07 +0100

    commit

--- src/Test.js
+++ src/Test.js
@@ -5,9 +5,11 @@ let turn='X';
 let game=[['#','#','#'],
           ['#','#','#'],
           ['#','#','#']]
+let Win=false;
 const handleClick = (e) => {
     
-    if(e.target.textContent===''){
+    if(e.target.textContent==='' && !Win){
+        e.target.style.color=(turn==='X')?'green':'red';
         e.target.textContent=turn
  turn=(turn==='X')?'O':'X';
  document.getElementsByClassName('turn')[0].textContent=`Player ${turn}'s turn`
@@ -15,11 +17,15 @@ const handleClick = (e) => {
  let x=Math.floor(index/3)
  let y=index%3
  console.log(x,y)
-game[x][y]=e.target.textContent
+game[x][y]=e.target.textContent;
+
 if(isWon(x,y)){
-    console.log("win")
+     document.getElementsByClassName('turn')[0].textContent=`Player ${game[x][y]} is won`
+     Win=true
+
+}else{
+    document.getElementsByClassName('turn')[0].textContent=`Player ${turn}'s turn`
 }
-console.log(game)
        }
    };
    function indexOf(array,object){
@@ -28,27 +34,56 @@ console.log(game)
             return i;
     }
    }
+ 
    function isWon(x,y){
-return (check(game[x] ) || check(extract(y,game)))
+    console.log(check(game[x]) ,check(extract_Y(y,game)) ,check(extract_D1(game)),check(extract_D2(game)))
+return (check(game[x]) || check(extract_Y(y,game)) || check(extract_D1(game)) || check(extract_D2(game)) )
     
 }
   
    function check(array){
-    for(let i=0;i<array.length-1;i++){
-        if(array[i]!=array[i+1]){
+    console.log("check"+array)
+    for(let j=0;j<2;j++){
+        if(array[j]!=array[j+1] || (array[j] ==='#')){
             return false;
         }
          }
          return true
         }
-   function extract(y,array){
+   function extract_Y(y,array){
     let newarry=[];
     for(let i=0;i<3;i++){
          newarry.push(array[i][y]) 
            }
          return newarry;
     }
-
+function extract_D1(array){
+    let newarry=[]
+    for(let i=0;i<3;i++){
+        newarry.push(array[i][i])
+    }
+    return newarry
+}
+function extract_D2(array){
+    let myarray=[]
+    for(let i=0;i<3;i++){
+        myarray.push(array[i][2-i])
+    }
+    console.log(myarray)
+    return myarray
+}
+function clear(){
+console.log(Win)
+Win=false
+game=[['#','#','#'],
+          ['#','#','#'],
+          ['#','#','#']]
+let cells=document.querySelectorAll('.cell');
+ document.getElementsByClassName('turn')[0].textContent=`Player ${turn}'s turn`
+cells.forEach(element => {
+    element.textContent=''
+});
+}
          return(<div className='tic_tac'>
             <h1>Tic Tac Toe</h1>
             <p className='turn'>Player {turn}'s turn</p>
@@ -63,6 +98,7 @@ return (check(game[x] ) || check(extract(y,game)))
                 <div className={props.cell} onClick={handleClick}></div>
                 <div className={props.cell} onClick={handleClick}></div>
                 </div>
+                <button onClick={clear}>restart</button>
                 </div>);
                 }
     
